e7efca87e420db6ae3c5b62eca56a18585f781c1
Kevin Castro <65462929+kevincastro04@users.noreply.github.com>
2022-11-10 23:37:17 -0600

    Instalando dependencias de navegación

--- whipala/App.js
+++ whipala/App.js
@@ -11,15 +11,34 @@ import React from 'react';
 import {
   Text,
   View,
+  StyleSheet
 } from 'react-native';
 
 
 const App = () => {
   return(
-    <View>
+    <View style = {styles.fondo}>
       <Text>Hola</Text>
+
+      <View style = {styles.dos}>
+      <Text>tertgerfgvfedgetr</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  fondo: {
+    backgroundColor: '#1c1b29',
+    flex: 1,
+    alignItems: 'center'
+  },
+
+  dos:{
+    backgroundColor:  '#282840',
+    height: 200,
+    with: 200
+  }
+})
 
 export default App;
